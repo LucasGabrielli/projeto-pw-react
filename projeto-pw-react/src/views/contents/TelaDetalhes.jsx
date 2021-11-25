b0c541bb3b4740778275d2812811637c826d1235
LucasGabrielli <lugujo@outlook.com>
2021-11-25 17:31:40 +0000

    commit

--- projeto-pw-react/src/views/contents/TelaDetalhes.jsx
+++ projeto-pw-react/src/views/contents/TelaDetalhes.jsx
@@ -6,6 +6,22 @@ const TelaDetalhes = props => (
         
         <h2>Ordem Paranormal é um universo de investigação paranormal criado por Rafael Lange (Cellbit), que teve seu início através de campanhas de RPG de mesa 
             em transmissões ao vivo na Twitch. Um jogo da saga, Ordem Paranormal: Enigma do Medo, tem previsão de lançamento para junho de 2022.</h2> 
+        <br />
+        <h2> A série "A Ordem Paranormal" se passa em um universo onde existem duas dimensões: o Normal e o Paranormal. O Normal é onde nós vivemos como humanos, 
+            enquanto o Paranormal é um lugar completamente diferente, onde tudo que parece impossível pode se tornar real, o que normalmente estaria associado a 
+            espíritos, demônios, entre outros tipos de anormalidades ligadas ao pós vida, por exemplo.</h2>
+        <br />
+        <h2> Esses dois locais são divididos por uma camada chamada de Membrana, que impede que o Normal e o Paranormal entrem em contato. No entanto, essa Membrana 
+            pode se enfraquecer com o aumento do medo do sobrenatural, levando monstros horrendos e muito perigosos a invadirem nossa realidade. Esse fenômeno geralmente
+             ocorre em locais bem específicos, propensos à percepção de medo, como hospitais abandonados, casas antigas e sanatórios desativados.</h2>
+        <br />
+        <h2>Ao redor do mundo inteiro diversos grupos se formaram, buscando enfraquecer a Membrana o máximo possível, a fim de destruí-la e levar à fusão das dimensões, 
+            com o objetivo de buscar seus próprios desejos ou, quem sabe, cumprindo um propósito maior. Os membros desses grupos são chamados de Ocultistas (nessa época, 
+            Esoterroristas).</h2> 
+        <br />
+        <h2> Para impedir que o caos se instale, uma organização global secreta foi criada: a Ordo Realitas ("Ordem da Realidade", nessa época chamada de Ordo Veritatis, 
+            "Ordem da Verdade"). Pessoas comuns que vivem vidas duplas para impedir os atos Ocultistas, que têm como objetivo o aumento do contato entre a população com 
+            o paranormal, aumentando seu medo e, consequentemente, enfraquecendo a membrana e permitindo a interferência do paranormal.</h2> 
     </div>
 
 )
--- projeto-pw-react/src/views/contents/TelaSobre.jsx
+++ projeto-pw-react/src/views/contents/TelaSobre.jsx
@@ -3,9 +3,10 @@ import React from "react"
 const TelaSobre = props => (
     <div>
         <h1>Sobre</h1>
-        <h2>Site com navegação em React que terá como objetivo resumir brevemente os acontecimentos de Ordem Paranormal, com textos explicando a história de uma forma
-            simples e rápida. - Lucas Vinícius Gabrielli 2ºDSBB
-        </h2>
+        <h2>Site com navegação em React que terá como objetivo resumir brevemente do que se trata Ordem Paranormal, com textos explicando seu conceito de uma forma
+            simples e rápida. </h2>
+        <br />
+        <h2> Lucas Vinícius Gabrielli - 2ºDSBB</h2>
     </div>
 )
 
