43a66d33888e71cc6b6f04ecdf1397fd0bfcf0f0
LucasGabrielli <lugujo@outlook.com>
2021-11-25 17:05:24 +0000

    commit

--- projeto-pw-react/src/views/contents/TelaSobre.jsx
+++ projeto-pw-react/src/views/contents/TelaSobre.jsx
@@ -4,7 +4,7 @@ const TelaSobre = props => (
     <div>
         <h1>Sobre</h1>
         <h2>Site com navegação em React que terá como objetivo resumir brevemente os acontecimentos de Ordem Paranormal, com textos explicando a história de uma forma
-            simples e rápida.
+            simples e rápida. - Lucas Vinícius Gabrielli 2ºDSBB
         </h2>
     </div>
 )
